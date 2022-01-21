40ea5d3680fae002bf6ba450f5467297c809d8d5
CandidKilsborne <ap.piotrowski@outlook.com>
2022-01-20 20:57:27 -0500

    added edit form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,27 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update]
+
   def index
     @movies = Movie.all
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+private
+  def set_movie
     @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
   end
 end
